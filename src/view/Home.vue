cfb683b552efbe9156fe1f70798f95e7d64239ed
LiuKun <13655914562@163.com>
2023-10-09 04:47:39 +0800

    tailwind

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -4,7 +4,8 @@
   <div>
     <span class="title">liukun.github.io</span>
   </div>
-  <el-button type="primary" plain @click="">chatgpt</el-button>
+  <el-button type="primary" plain @click="" class="m-2">chatgpt</el-button>
+  <div class="w-24 h-20 bg-slate-700"></div>
 </template>
 
 
