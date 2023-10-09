1542f1937089fa5ebbdf5f7756265c596349950d
LiuKun <13655914562@163.com>
2023-10-09 18:23:25 +0800

    gpt3

--- src/utils/chat.ts
+++ src/utils/chat.ts
@@ -1,3 +1,21 @@
-export function send(){
-    
+import axios from 'axios'
+export async function send(content:string){
+    const res = await axios({
+        url: 'https://api.chatanywhere.com.cn/v1/chat/completions',
+        method: 'post',
+        data:{
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "user",
+                    content:content
+                }
+            ]
+        },
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer sk-biUFdHeJmhMPPFQF5QaMbANSnZW1VXfc9zBFM4wWNB34IpRW"
+        }
+    })
+    return res;
 }
--- src/view/gpt3.5.vue
+++ src/view/gpt3.5.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { send } from '../utils/chat';
 
 const content = ref('');
-const submit = () => {
-    console.log(content.value);
-
+const answer = ref('')
+const submit = async () => {
+    const result = await send(content.value);
+    answer.value = result.data.choices[0].message.content;
+    content.value = ''
 }
 </script>
 <template>
-    <span>3.5</span>
     <div id="answer">
-        
+        {{ answer }}
     </div>
     <div class="flex flex-col">
-        <textarea v-model=content cols="100" rows="4" class="px-4 border-2"></textarea>
+        <textarea v-model=content cols="100" rows="4" class="px-4 border-2 py-1"></textarea>
         <button @click="submit" class="my-2">发送</button>
     </div>
 </template>
