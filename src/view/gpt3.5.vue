20dde7aa6b7e418c1fac34af3a216ccbf5603186
LiuKun <13655914562@163.com>
2023-10-09 18:00:34 +0800

    axios pinia

--- src/view/gpt3.5.vue
+++ src/view/gpt3.5.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const content = ref('');
+const submit = () => {
+    console.log(content.value);
+
+}
 </script>
 <template>
     <span>3.5</span>
+    <div id="answer">
+        
+    </div>
+    <div class="flex flex-col">
+        <textarea v-model=content cols="100" rows="4" class="px-4 border-2"></textarea>
+        <button @click="submit" class="my-2">发送</button>
+    </div>
 </template>
 
 
